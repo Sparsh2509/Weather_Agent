61db5b854e24be54e06ddb769956e33c6605355d
Sparsh2509 <sparshgupta250904@gmail.com>
2026-08-12 20:19:36 +0530

    graph file added

---
+++ graph.py
@@ -0,0 +1,48 @@
+from langgraph.graph import (
+    StateGraph,
+    MessagesState,
+    START
+)
+
+from langgraph.prebuilt import (
+    ToolNode,
+    tools_condition
+)
+
+from agent import agent
+from tools import tools
+
+
+graph = StateGraph(MessagesState)
+
+
+graph.add_node(
+    "agent",
+    agent
+)
+
+graph.add_node(
+    "tools",
+    ToolNode(tools)
+)
+
+
+graph.add_edge(
+    START,
+    "agent"
+)
+
+
+graph.add_conditional_edges(
+    "agent",
+    tools_condition
+)
+
+
+graph.add_edge(
+    "tools",
+    "agent"
+)
+
+
+app = graph.compile()
