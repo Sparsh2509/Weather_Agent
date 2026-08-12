d9851ffd1d7531d1ab7feae171cb22214ef37822
Sparsh2509 <sparshgupta250904@gmail.com>
2026-08-12 20:17:47 +0530

    agent file added

---
+++ agent.py
@@ -0,0 +1,34 @@
+import os
+
+from langchain_groq import ChatGroq
+from langchain_core.messages import SystemMessage
+
+from langgraph.graph import MessagesState
+
+from tools import tools
+from prompts import SYSTEM_PROMPT
+
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0,
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+
+llm_with_tools = llm.bind_tools(tools)
+
+
+def agent(state: MessagesState):
+
+    messages = [
+        SystemMessage(content=SYSTEM_PROMPT)
+    ] + state["messages"]
+
+    response = llm_with_tools.invoke(messages)
+
+    print("TOOL CALLS:", response.tool_calls)
+
+    return {
+        "messages": [response]
+    }
